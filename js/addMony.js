11c560e44615fbfde2826c8e2cd7dd4169341a89
golap-raj <golapraj47@gmail.com>
2025-02-25 17:04:47 +0600

    resolve

--- js/addMony.js
+++ js/addMony.js
@@ -1,7 +1,20 @@
 document.getElementById("Add_money").addEventListener("click", function(event){
     event.preventDefault()
-    const Ammount = document.getElementById("ammount").value
-    console.log(Ammount)
-    
+    const Ammount = document.getElementById("Ammount").value
+    const convertAmmount = parseFloat(Ammount);
+
+    const pin =  document.getElementById("pin").value;
+    const convertPin = parseInt(pin)
+
+    const mainBalnce = document.getElementById("main_balnce").innerText;
+const convertMainBlance = parseFloat(mainBalnce)
+
+    if(convertPin === 1234){
+        const sum = convertMainBlance + convertAmmount;
+        document.getElementById("main_balnce").innerHTML = sum
+    }
+    else{
+        console.log("pin thik nha")
+    }
 
 })
---
+++ js/text/text.js
@@ -0,0 +1,8 @@
+document.getElementById("Bangladesh_btn").addEventListener("click", function(){
+    document.getElementById("usa").style.display="none";
+    document.getElementById("bd").style.display="block"
+})
+document.getElementById("USA_btn").addEventListener("click", function(){
+    document.getElementById("usa").style.display="block";
+    document.getElementById("bd").style.display="none"
+})
